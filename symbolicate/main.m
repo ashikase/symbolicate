1cabb94877160e2e080fea648f452f3c6d81065e
Lance Fetters <gaizin@gmail.com>
2013-02-19 03:18:28 +0900

    Added command-line option to write output to file

--- symbolicate/main.m
+++ symbolicate/main.m
@@ -18,42 +18,62 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 
-#import <UIKit/UIKit.h>
-#import "symbolicate.h"
+#include <getopt.h>
 #include <string.h>
 
-int main (int argc, char *argv[]) {
+#import "symbolicate.h"
+
+void print_usage() {
+    fprintf(stderr,
+            "Usage: symbolicate [<options>] <file>\n"
+            "\n"
+            "Options:\n"
+            "    -o <file>  Write output to file instead of to stdout.\n"
+            "\n"
+           );
+}
+
+int main(int argc, char *argv[]) {
     int ret = 1;
 
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 
-    if (argc > 1) {
-        NSString *filepath = [NSString stringWithUTF8String:argv[1]];
-        NSString *content = [NSString stringWithContentsOfFile:filepath encoding:NSUTF8StringEncoding error:NULL];
-        if (content != nil) {
-            NSString *result = symbolicate(content, nil);
-            if (result != nil) {
-                printf("%s\n", [result UTF8String]);
-                ret = 0;
+    if (argc == 1) {
+        print_usage();
+    } else {
+        const char *outputFile = NULL;
+
+        int c;
+        while ((c = getopt (argc, argv, "o:")) != -1) {
+            switch (c) {
+                case 'o':
+                    outputFile = optarg;
+                    break;
+                default:
+                    break;
             }
         }
-#if 0
-        NSString *symbolicatedFile = [[[file stringByDeletingPathExtension] stringByAppendingString:@".symbolicated.plist"] retain];
-        NSString *lines_to_write = [file_lines componentsJoinedByString:@"\n"];
-        [file_lines release];
-        if (![lines_to_write writeToFile:symbolicatedFile atomically:NO encoding:NSUTF8StringEncoding error:NULL]) {
-            char temp_name[strlen("/tmp/crash_reporter.XXXXXX") + 1];
-            memcpy(temp_name, "/tmp/crash_reporter.XXXXXX", sizeof(temp_name));
-            mktemp(temp_name);
-            [lines_to_write writeToFile:[NSString stringWithUTF8String:temp_name] atomically:NO encoding:NSUTF8StringEncoding error:NULL];
-            const char *actual_sym_file_path = [[curPath stringByAppendingPathComponent:symbolicatedFile] UTF8String];
-            const char *actual_file_path = [[curPath stringByAppendingPathComponent:file] UTF8String];
 
-            exec_move_as_root(temp_name, actual_sym_file_path, actual_file_path);
+        const char *inputFile = (optind < argc) ? argv[optind] : NULL;
+        if (inputFile == NULL) {
+            print_usage();
+        } else {
+            NSString *filepath = [NSString stringWithUTF8String:inputFile];
+            NSString *content = [NSString stringWithContentsOfFile:filepath encoding:NSUTF8StringEncoding error:NULL];
+            if (content != nil) {
+                NSString *result = symbolicate(content, nil);
+                if (result != nil) {
+                    if (outputFile != NULL) {
+                        NSString *path = [NSString stringWithUTF8String:outputFile];
+                        [content writeToFile:path atomically:NO encoding:NSUTF8StringEncoding error:NULL];
+                        printf("Result written to %s.\n", outputFile);
+                    } else {
+                        printf("%s\n", [result UTF8String]);
+                    }
+                    ret = 0;
+                }
+            }
         }
-
-        printf("Result written to %s.\n", [result UTF8String]);
-#endif
     }
 
     [pool drain];
