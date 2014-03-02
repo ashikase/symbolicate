0d79186ea6ce6cdf281d77054c5f4b92066cee4e
Lance Fetters <gaizin@gmail.com>
2014-03-02 21:31:47 +0900

    Added fetchSymbolInfo()

--- symbolicate.mm
+++ symbolicate.mm
@@ -88,6 +88,102 @@ static BacktraceInfo *extractBacktraceInfo(NSString *line) {
     return [bti autorelease];
 }
 
+SymbolInfo *fetchSymbolInfo(BinaryInfo *bi, uint64_t address, NSDictionary *symbolMap) {
+    SymbolInfo *symbolInfo = nil;
+
+    VMUMachOHeader *header = [bi header];
+    if (header != nil) {
+        address += bi->slide;
+        VMUSourceInfo *srcInfo = [bi->owner sourceInfoForAddress:address];
+        if (srcInfo != nil) {
+            // Store source file name and line number.
+            symbolInfo = [SymbolInfo new];
+            [symbolInfo setSourcePath:[srcInfo path]];
+            [symbolInfo setSourceLineNumber:[srcInfo lineNumber]];
+        } else {
+            // Determine symbol address.
+            // NOTE: Only possible if LC_FUNCTION_STARTS exists in the binary.
+            uint64_t symbolAddress = 0;
+            NSUInteger count = [bi->symbolAddresses count];
+            if (count != 0) {
+                NSNumber *targetAddress = [[NSNumber alloc] initWithUnsignedLongLong:address];
+                CFIndex matchIndex = CFArrayBSearchValues((CFArrayRef)bi->symbolAddresses, CFRangeMake(0, count), targetAddress, (CFComparatorFunction)reversedCompareNSNumber, NULL);
+                [targetAddress release];
+                if (matchIndex < (CFIndex)count) {
+                    symbolAddress = [[bi->symbolAddresses objectAtIndex:matchIndex] unsignedLongLongValue];
+                }
+            }
+
+            // Attempt to retrieve symbol name and hex offset.
+            NSString *name = nil;
+            uint64_t offset = 0;
+            VMUSymbol *symbol = [bi->owner symbolForAddress:address];
+            if (symbol != nil && ([symbol addressRange].location == (symbolAddress & ~1) || symbolAddress == 0)) {
+                name = [symbol name];
+                if ([name isEqualToString:@"<redacted>"]) {
+                    BOOL hasHeaderFromSharedCacheWithPath = [VMUMemory_File respondsToSelector:@selector(headerFromSharedCacheWithPath:)];
+                    if (hasHeaderFromSharedCacheWithPath) {
+                        NSString *localName = nameForLocalSymbol([header address], [symbol addressRange].location);
+                        if (localName != nil) {
+                            name = localName;
+                        } else {
+                            fprintf(stderr, "Unable to determine name for: %s, 0x%08llx\n", [[bi path] UTF8String], [symbol addressRange].location);
+                        }
+                    }
+                }
+                // Attempt to demangle name
+                // NOTE: It seems that Apple's demangler fails for some
+                //       names, so we attempt to do it ourselves.
+                name = demangle(name);
+                offset = address - [symbol addressRange].location;
+            } else if (symbolMap != nil) {
+                for (NSNumber *number in [[[symbolMap allKeys] sortedArrayUsingSelector:@selector(compare:)] reverseObjectEnumerator]) {
+                    uint64_t mapSymbolAddress = [number unsignedLongLongValue];
+                    if (address > mapSymbolAddress) {
+                        name = demangle([symbolMap objectForKey:number]);
+                        offset = address - mapSymbolAddress;
+                        break;
+                    }
+                }
+            } else if (!bi->encrypted) {
+                // Determine methods, attempt to match with symbol address.
+                if (symbolAddress != 0) {
+                    MethodInfo *method = nil;
+                    NSArray *methods = [bi methods];
+                    count = [methods count];
+                    if (count != 0) {
+                        MethodInfo *targetMethod = [[MethodInfo alloc] init];
+                        targetMethod->address = address;
+                        CFIndex matchIndex = CFArrayBSearchValues((CFArrayRef)methods, CFRangeMake(0, count), targetMethod, (CFComparatorFunction)reversedCompareMethodInfos, NULL);
+                        [targetMethod release];
+
+                        if (matchIndex < (CFIndex)count) {
+                            method = [methods objectAtIndex:matchIndex];
+                        }
+                    }
+
+                    if (method != nil && method->address >= symbolAddress) {
+                        name = method->name;
+                        offset = address - method->address;
+                    } else {
+                        uint64_t textStart = [[header segmentNamed:@"__TEXT"] vmaddr];
+                        name = [NSString stringWithFormat:@"0x%08llx", (symbolAddress - textStart)];
+                        offset = address - symbolAddress;
+                    }
+                }
+            }
+
+            if (name != nil) {
+                symbolInfo = [SymbolInfo new];
+                [symbolInfo setName:name];
+                [symbolInfo setOffset:offset];
+            }
+        }
+    }
+
+    return symbolInfo;
+}
+
 NSString *symbolicate(NSString *content, NSDictionary *symbolMaps, unsigned progressStepping, NSArray **blameInfo) {
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 
@@ -202,7 +298,6 @@ NSString *symbolicate(NSString *content, NSDictionary *symbolMaps, unsigned prog
 
     NSUInteger i = 0;
     BOOL isCrashing = NO;
-    BOOL hasHeaderFromSharedCacheWithPath = [VMUMemory_File respondsToSelector:@selector(headerFromSharedCacheWithPath:)];
     Class $BinaryInfo = [BinaryInfo class];
     NSUInteger total_lines = [extraInfoArray count];
     NSUInteger last_percent = 0;
@@ -261,99 +356,9 @@ NSString *symbolicate(NSString *content, NSDictionary *symbolMaps, unsigned prog
                     [bi release];
                 }
 
-                // Add source/symbol information to the end of the output line.
-                SymbolInfo *symbolInfo = nil;
-                VMUMachOHeader *header = [bi header];
-                if (header != nil) {
-                    uint64_t address = bti->address + bi->slide;
-                    VMUSourceInfo *srcInfo = [bi->owner sourceInfoForAddress:address];
-                    if (srcInfo != nil) {
-                        // Store source file name and line number.
-                        symbolInfo = [SymbolInfo new];
-                        [symbolInfo setSourcePath:[srcInfo path]];
-                        [symbolInfo setSourceLineNumber:[srcInfo lineNumber]];
-                    } else {
-                        // Determine symbol address.
-                        // NOTE: Only possible if LC_FUNCTION_STARTS exists in the binary.
-                        uint64_t symbolAddress = 0;
-                        NSUInteger count = [bi->symbolAddresses count];
-                        if (count != 0) {
-                            NSNumber *targetAddress = [[NSNumber alloc] initWithUnsignedLongLong:address];
-                            CFIndex matchIndex = CFArrayBSearchValues((CFArrayRef)bi->symbolAddresses, CFRangeMake(0, count), targetAddress, (CFComparatorFunction)reversedCompareNSNumber, NULL);
-                            [targetAddress release];
-                            if (matchIndex < (CFIndex)count) {
-                                symbolAddress = [[bi->symbolAddresses objectAtIndex:matchIndex] unsignedLongLongValue];
-                            }
-                        }
-
-                        // Attempt to retrieve symbol name and hex offset.
-                        NSString *name = nil;
-                        uint64_t offset = 0;
-                        VMUSymbol *symbol = [bi->owner symbolForAddress:address];
-                        if (symbol != nil && ([symbol addressRange].location == (symbolAddress & ~1) || symbolAddress == 0)) {
-                            //if (alreadySymbolicated) {
-                            //    goto skip_this_line;
-                            //}
-                            name = [symbol name];
-                            if ([name isEqualToString:@"<redacted>"] && hasHeaderFromSharedCacheWithPath) {
-                                NSString *localName = nameForLocalSymbol([header address], [symbol addressRange].location);
-                                if (localName != nil) {
-                                    name = localName;
-                                } else {
-                                    fprintf(stderr, "Unable to determine name for: %s, 0x%08llx\n", [[bi path] UTF8String], [symbol addressRange].location);
-                                }
-                            }
-                            // Attempt to demangle name
-                            // NOTE: It seems that Apple's demangler fails for some
-                            //       names, so we attempt to do it ourselves.
-                            name = demangle(name);
-                            offset = address - [symbol addressRange].location;
-                        } else if (NSDictionary *map = [symbolMaps objectForKey:[bi path]]) {
-                            for (NSNumber *number in [[[map allKeys] sortedArrayUsingSelector:@selector(compare:)] reverseObjectEnumerator]) {
-                                uint64_t mapSymbolAddress = [number unsignedLongLongValue];
-                                if (address > mapSymbolAddress) {
-                                    name = demangle([map objectForKey:number]);
-                                    offset = address - mapSymbolAddress;
-                                    break;
-                                }
-                            }
-                        } else if (!bi->encrypted) {
-                            // Determine methods, attempt to match with symbol address.
-                            if (symbolAddress != 0) {
-                                MethodInfo *method = nil;
-                                NSArray *methods = [bi methods];
-                                count = [methods count];
-                                if (count != 0) {
-                                    MethodInfo *targetMethod = [[MethodInfo alloc] init];
-                                    targetMethod->address = address;
-                                    CFIndex matchIndex = CFArrayBSearchValues((CFArrayRef)methods, CFRangeMake(0, count), targetMethod, (CFComparatorFunction)reversedCompareMethodInfos, NULL);
-                                    [targetMethod release];
-
-                                    if (matchIndex < (CFIndex)count) {
-                                        method = [methods objectAtIndex:matchIndex];
-                                    }
-                                }
-
-                                if (method != nil && method->address >= symbolAddress) {
-                                    name = method->name;
-                                    offset = address - method->address;
-                                } else {
-                                    uint64_t textStart = [[header segmentNamed:@"__TEXT"] vmaddr];
-                                    name = [NSString stringWithFormat:@"0x%08llx", (symbolAddress - textStart)];
-                                    offset = address - symbolAddress;
-                                }
-                            }
-                        }
-
-                        if (name != nil) {
-                            symbolInfo = [SymbolInfo new];
-                            [symbolInfo setName:name];
-                            [symbolInfo setOffset:offset];
-                        }
-                    }
-                }
-
                 NSString *lineComment = nil;
+                NSDictionary *symbolMap = [symbolMaps objectForKey:[bi path]];
+                SymbolInfo *symbolInfo = fetchSymbolInfo(bi, bti->address, symbolMap);
                 if (symbolInfo != nil) {
                     NSString *sourcePath = [symbolInfo sourcePath];
                     if (sourcePath != nil) {
